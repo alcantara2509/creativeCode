935a6d42a18379be9bc3e249f3392f3d4ce845a2
alcantara2509 <alcantara2509@gmail.com>
2021-06-15 15:17:42 -0300

    configurando eslint airbnb

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <form action="">
+      <input required type="email" />
+      <input required type="password" />
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
+
+export default Login;
